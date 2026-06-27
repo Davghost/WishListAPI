f7554fc55adb15baeed3fad9d11c37f672cb9c2e
Davghost <acrookedfork@gmail.com>
2026-06-27 20:22:44 -0300

    Add all routes CRUD about manage the wishlist and add documentation spectree swagger/redoc

---
+++ controllers/wishlist_item.py
@@ -0,0 +1,117 @@
+from factory import db, api
+from spectree import Response
+from flask import Blueprint, jsonify
+from sqlalchemy import select
+from models.wishlist_item import WishListItem
+from flask.globals import request
+
+wishlist_controller = Blueprint("wishlist_controller", __name__, url_prefix="/api/wishlist")
+
+@wishlist_controller.get("/")
+@api.validate(resp=Response(HTTP_200=None, HTTP_404=None), tags=["wishlist"])
+def getall_itens():
+    """
+    Return all itens
+    """
+    itens = db.session.scalars(select(WishListItem)).all()
+
+    if not itens:
+        return {"msg": "There is no itens"}, 404
+    
+    return jsonify(
+        [
+            {
+                "id": item.id,
+                "name": item.name,
+                "description": item.description if item.description else None,
+                "link": item.link if item.link else None,
+                "purchased": item.purchased,
+                "sort_order": item.sort_order if item.sort_order else None,
+            }
+            for item in itens
+        ] 
+    ), 200
+
+@wishlist_controller.get("/<int:item_id>")
+@api.validate(resp=Response(HTTP_200=None, HTTP_404=None), tags=["wishlist"])
+def get_item(item_id):
+    """
+    Return an item
+    """
+    item = db.session.get(WishListItem, item_id)
+
+    if item is None:
+        return {"msg": f"There is no item with id {item_id}"}, 404
+    
+    return {
+        "id": item.id,
+        "name": item.name,
+        "description": item.description if item.description else None,
+        "link": item.link if item.link else None,
+        "purchased": item.purchased,
+        "sort_order": item.sort_order if item.sort_order else None,
+    }, 200
+
+@wishlist_controller.post("/")
+@api.validate(resp=Response(HTTP_200=None), tags=["wishlist"])
+def post_item():
+    """
+    Create an item
+    """
+    data = request.json
+
+    if db.session.scalars(
+        select(WishListItem).filter_by(name=data["name"])
+    ).first():
+        return {"msg": "name not available"}, 409
+    
+    item = WishListItem(
+        name = data["name"],
+        description = data["description"] if "description" in data else None,
+        link = data["link"] if "link" in data else None,
+        purchased = data["purchased"],
+        sort_order = data["sort_order"] if "sort_order" in data else None
+    )
+
+    db.session.add(item)
+    db.session.commit()
+
+    return {"msg": "Item added in wishlist wish success"}, 201
+
+@wishlist_controller.put("/<int:item_id>")
+@api.validate(resp=Response(HTTP_200=None, HTTP_404=None), tags=["wishlist"])
+def put_item(item_id):
+    """
+    Update an item
+    """
+    item = db.session.get(WishListItem, item_id)
+
+    if not item:
+        return {"msg": f"There is no item with id {item_id}"}, 404
+
+    data = request.json
+    
+    item.name = data["name"]
+    item.description = data["description"] if "description" in data else None
+    item.link = data["link"] if "link" in data else None
+    item.purchased = data["purchased"]
+    item.sort_order = data["sort_order"] if "sort_order" in data else None
+
+    db.session.commit()
+    return {"msg": "Item successfully updated"}, 200
+
+@wishlist_controller.delete("/<int:item_id>")
+@api.validate(resp=Response(HTTP_200=None, HTTP_404=None), tags=["wishlist"])
+def delete_item(item_id):
+    """
+    Delete an item
+    """
+    item = db.session.get(WishListItem, item_id)
+
+    if not item:
+        return {"msg": f"There is no item with id {item_id}"}, 404
+    
+    db.session.delete(item)
+    db.session.commit()
+
+    return {"msg": "Item successfully deleted"}, 200
--- factory.py
+++ factory.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from spectree import SpecTree
 
 db = SQLAlchemy()
 migrate = Migrate()
+api = SpecTree("flask", title="WishListAPI", version="v.1.0", path="docs")
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +16,11 @@ def create_app():
     from models import WishListItem
 
     migrate.init_app(app, db)
+
+    from controllers import wishlist_controller
+    app.register_blueprint(wishlist_controller)
+
+    api.register(app)
 
     @app.get("/")
     def home():
